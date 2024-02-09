1f5685e7be5391550b57406a07c591ddb9d22927
Larissa <larissa.mikkers@hotmail.com>
2024-02-09 11:01:57 +0100

    js aangepast

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,24 @@
-// var card = document.getElementById("card")
+// var element = document.getElementById("card");
+// var button = document.querySelectorAll("button");
+
+
+
+// function turn() {
+//     element.classList.toggle("turned");
+//   }
+
+var card = document.querySelector('.cardContainer')
+var button = document.querySelector('button');
+
+var buttons = document.querySelectorAll('button');
+
+// button.addEventListener('click', turn);
 
 function turn() {
-    var element = document.getElementById("card")
-    element.classList.toggle('turned');
-  }
+    console.log('toggle card');
+    card.classList.toggle('turned');
+}
+
+buttons.forEach(function(buttonTurn) {
+  buttonTurn.addEventListener('click', turn);
+});
